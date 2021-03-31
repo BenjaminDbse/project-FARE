93dab7f22de983ca6e2b9c3fe19de29b7a3405d3
benjamindbse <benjideboise@gmail.com>
2021-03-31 08:40:53 +0200

    resolve bug prod

--- src/Controller/GraphController.php
+++ src/Controller/GraphController.php
@@ -61,7 +61,30 @@ class GraphController extends AbstractController
             $session->set('adr', $filterAdr);
 
             $dataFilter = $dataRepository->findByLikeAdr($import->getId(), $filterAdr);
-            include __DIR__ . '/../include/resultToRequest.php';
+            foreach ($dataFilter as $data) {
+                $delta1[] = $data->getDelta1();
+                $delta2[] = $data->getDelta2();
+                $ratioFilter[] = $data->getFilterRatio();
+                $slopeTemperatureCorrection[] = $data->getSlopeTemperatureCorrection();
+                $rawCo[] = $data->getRawCo();
+                $coCorrection[] = $data->getCoCorrection();
+                $temperatureCorrection[] = $data->getTemperatureCorrection();
+                $datetime[] = date_format($data->getDatetime(), 'd-m-Y  /  H:i:s');
+                $alarm[] = $data->getAlarm();
+                $status[] = $data->getStatus();
+            }
+            for ($i = 0 ; $i < count($status) ; $i++) {
+                if ((key_exists($i + 1, $status)) && ($status[$i] != $status[$i + 1])) {
+                    $condition[$i] = [$status[$i], $status[$i + 1], $datetime[$i + 1]];
+                }
+            }
+            $max = [
+                max($delta1),
+                max($delta2),
+                max($slopeTemperatureCorrection),
+                max($rawCo),
+                max($temperatureCorrection),
+            ];
         }
         if (isset($_POST['date']) && !empty($_POST['limit'])) {
             $session = $request->getSession()->all();
@@ -76,7 +99,30 @@ class GraphController extends AbstractController
             $userChoiceDate = join(' ', $userChoiceDate);
             $userChoiceLimit = $_POST['limit'];
             $dataFilter = $dataRepository->findByDateToLimit($import->getId(), $filterAdr, $userChoiceDate, $userChoiceLimit);
-            include __DIR__ . '/../include/resultToRequest.php';
+            foreach ($dataFilter as $data) {
+                $delta1[] = $data->getDelta1();
+                $delta2[] = $data->getDelta2();
+                $ratioFilter[] = $data->getFilterRatio();
+                $slopeTemperatureCorrection[] = $data->getSlopeTemperatureCorrection();
+                $rawCo[] = $data->getRawCo();
+                $coCorrection[] = $data->getCoCorrection();
+                $temperatureCorrection[] = $data->getTemperatureCorrection();
+                $datetime[] = date_format($data->getDatetime(), 'd-m-Y  /  H:i:s');
+                $alarm[] = $data->getAlarm();
+                $status[] = $data->getStatus();
+            }
+            for ($i = 0 ; $i < count($status) ; $i++) {
+                if ((key_exists($i + 1, $status)) && ($status[$i] != $status[$i + 1])) {
+                    $condition[$i] = [$status[$i], $status[$i + 1], $datetime[$i + 1]];
+                }
+            }
+            $max = [
+                max($delta1),
+                max($delta2),
+                max($slopeTemperatureCorrection),
+                max($rawCo),
+                max($temperatureCorrection),
+            ];
         }
         for ($i = 0; $i < count($alarm); $i++) {
             if ($alarm[$i] >= 1) {
