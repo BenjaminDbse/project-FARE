86ac754d66f366732cc331f992c4f77448e97f1f
BenjaminDbse <67733668+BenjaminDbse@users.noreply.github.com>
2021-04-08 16:27:40 +0200

    Update Version20210408081154.php

--- migrations/Version20210408081154.php
+++ migrations/Version20210408081154.php
@@ -21,13 +21,11 @@ final class Version20210408081154 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE algo (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, coef1ratio DOUBLE PRECISION NOT NULL, coef1tmp DOUBLE PRECISION NOT NULL, coef2tmp DOUBLE PRECISION NOT NULL, coef2ratio DOUBLE PRECISION NOT NULL, xtmp DOUBLE PRECISION NOT NULL, xratio DOUBLE PRECISION NOT NULL, ytmp DOUBLE PRECISION NOT NULL, yratio DOUBLE PRECISION NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, firstname VARCHAR(50) NOT NULL, lastname VARCHAR(50) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('DROP TABLE algo');
-        $this->addSql('DROP TABLE user');
     }
 }
